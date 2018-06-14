cc18bc287c9cb7dc8f8dcf3b2a33276db8429ba3
Victor Arbues <arbuesv@TMGM-C02TNDTMG8WN.local>
2018-06-14 23:55:05 +0100

    passing test for shopping holding a list of fruits

--- src/main/scala/org/shop/package.scala
+++ src/main/scala/org/shop/package.scala
@@ -4,4 +4,6 @@ package object shop {
   sealed trait Fruit { val cost: BigDecimal; val name: String }
   case object Apple extends Fruit { val cost = 0.6; val name = "apple" }
   case object Orange extends Fruit { val cost = 0.25; val name = "orange" }
+
+  case class ShoppingCart(fruits: Seq[Fruit])
 }
