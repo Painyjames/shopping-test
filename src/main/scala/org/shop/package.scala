b5b937756c2f88beb294a21b22e68546b3a24938
Victor Arbues <arbuesv@TMGM-C02TNDTMG8WN.local>
2018-06-15 00:12:48 +0100

    added a convert method for list of strings as input

--- src/main/scala/org/shop/package.scala
+++ src/main/scala/org/shop/package.scala
@@ -5,6 +5,10 @@ package object shop {
   case object Apple extends Fruit { val cost = 0.6; val name = "apple" }
   case object Orange extends Fruit { val cost = 0.25; val name = "orange" }
 
+  val validFruits = Seq(Apple, Orange)
+  implicit def convert(fruits: Seq[String]): Seq[Fruit] =
+    fruits.flatMap(fruit => validFruits.find(_.name == fruit))
+
   case class ShoppingCart(fruits: Seq[Fruit]) {
     lazy val totalCost: BigDecimal =
       fruits.foldLeft(BigDecimal(0))(_ + _.cost)
