7d7e495721c4469e41916c509fc8ab4e6f11428b
Victor Arbues <arbuesv@TMGM-C02TNDTMG8WN.local>
2018-06-15 00:01:41 +0100

    passing test for the shopping cart total cost calculation

--- src/main/scala/org/shop/package.scala
+++ src/main/scala/org/shop/package.scala
@@ -5,5 +5,8 @@ package object shop {
   case object Apple extends Fruit { val cost = 0.6; val name = "apple" }
   case object Orange extends Fruit { val cost = 0.25; val name = "orange" }
 
-  case class ShoppingCart(fruits: Seq[Fruit])
+  case class ShoppingCart(fruits: Seq[Fruit]) {
+    lazy val totalCost: BigDecimal =
+      fruits.foldLeft(BigDecimal(0))(_ + _.cost)
+  }
 }
