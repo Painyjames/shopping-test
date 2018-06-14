dbce8c06ae352b6be9d8c41749e05b894fdd86b8
Victor Arbues <arbuesv@TMGM-C02TNDTMG8WN.local>
2018-06-14 23:46:49 +0100

    passing fruit costs tests

---
+++ src/main/scala/org/shop/package.scala
@@ -0,0 +1,7 @@
+package org
+
+package object shop {
+  sealed trait Fruit { val cost: BigDecimal; val name: String }
+  case object Apple extends Fruit { val cost = 0.6; val name = "apple" }
+  case object Orange extends Fruit { val cost = 0.25; val name = "orange" }
+}
